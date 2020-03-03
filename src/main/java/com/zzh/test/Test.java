d316ec11e22f1b2331d016db9241f2f7b405ebd4
473011317 <473011317@qq.com>
2020-03-03 18:48:01 +0800

    第一次提交23

---
+++ src/main/java/com/zzh/test/Test.java
@@ -0,0 +1,13 @@
+package com.zzh.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(Test.class);
+		logger.info("hello");
+	}
+
+}
